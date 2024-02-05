7c9e42869b9a5adc23daa7947e6df53016152360
kaiqi <zhang.kaiqi1@northeastern.edu>
2024-02-04 19:35:02 -0800

    first commit

---
+++ app/profile/loading.jsx
@@ -0,0 +1,15 @@
+import Image from "next/image";
+const Loading = () => {
+  return (
+    <div>
+      <Image
+        src="assets/icons/loader.svg"
+        width={50}
+        height={50}
+        alt="loader"
+        className="object-contain"
+      />
+    </div>
+  );
+};
+export default Loading;
